2ba9fad3594983a70230b5a687797343e0335a17
mohammed shahnawaz <shah4corp@gmail.com>
2023-08-15 12:37:28 +0530

    15082023 commit 3

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -1,47 +1,58 @@
 package stepDefinitions;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.*;
+import junit.framework.Assert;
+import pageObjects.LoginPage;
 
 public class Steps {
+	public WebDriver driver;
+	LoginPage lp = new LoginPage(driver);
+	
 	@Given("User Launch Chrome Browser")
 	public void user_launch_chrome_browser() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    driver = new ChromeDriver();
+	    driver.manage().window().maximize();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 
 	@When("User opens URL {string}")
-	public void user_opens_url(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void user_opens_url(String baseURL) {
+	    driver.get(baseURL);
 	}
 
 	@When("User enters Email as {string} and password as {string}")
-	public void user_enters_email_as_and_password_as(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void user_enters_email_as_and_password_as(String uname, String pwd) {
+	    lp.setUserName(uname);
+	    lp.setPassword(pwd);
 	}
 
 	@When("Click on Login")
 	public void click_on_login() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    lp.clickLogin();
 	}
 
 	@Then("Page Title should be {string}")
-	public void page_title_should_be(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void page_title_should_be(String title) {
+	    if(driver.getPageSource().contains("Login was unsuccessful")) {
+	    	driver.close();
+	    	Assert.assertFalse(true);
+	    }else {
+	    	Assert.assertEquals(title, driver.getTitle());
+	    }
 	}
 
 	@When("Click on Logout link")
 	public void click_on_logout_link() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    lp.clickLogout();
 	}
 
 	@Then("close browser")
 	public void close_browser() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    driver.quit();
 	}
 }
