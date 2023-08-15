ffed3864e4cb66951cac12e53b7c5833d04cfaf1
mohammed shahnawaz <shah4corp@gmail.com>
2023-08-15 11:05:29 +0530

    15082023 first commit

---
+++ src/test/java/stepDefinitions/Steps.java
@@ -0,0 +1,47 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class Steps {
+	@Given("User Launch Chrome Browser")
+	public void user_launch_chrome_browser() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User opens URL {string}")
+	public void user_opens_url(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User enters Email as {string} and password as {string}")
+	public void user_enters_email_as_and_password_as(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Click on Login")
+	public void click_on_login() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Page Title should be {string}")
+	public void page_title_should_be(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Click on Logout link")
+	public void click_on_logout_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("close browser")
+	public void close_browser() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
